165ed3e85e07b05251864b4ec06a1bd5964b3a6e
ReeceTeylan <rixteylan24@gmail.com>
2024-05-08 19:51:23 +0800

    Add pseudocodes

--- program_5.py
+++ program_5.py
@@ -1,2 +1,10 @@
 #Create a Simple App Calculator
 
+# Integrate tkinter to the code
+# The application will ask the user to choose one of the four math operations
+# The application will ask the user for two numbers
+# Display the result
+# The application will ask if the user wants to try again or not
+# If yes, repeat Step 1
+# If no, Display “Thank you!” and the program will exit 
+# Use Python Function and appropriate Exceptions to capture errors during runtime
